fd68a29622efa8ccb5f79a60042405e7756817cd
Egor Saveliev <esavelievCode@gmail.com>
2023-04-29 01:47:36 +0300

    New packages and add API

--- bebrochkaTulahack.cpp
+++ bebrochkaTulahack.cpp
@@ -1,55 +1,99 @@
-﻿#include <iostream>
-#include <string>
-#include <boost/beast.hpp>
+﻿#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
 #include <boost/asio.hpp>
+#include <boost/json.hpp>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <memory>
+#include <string>
 
-using namespace boost::asio;
-ip::tcp::endpoint endpnt{ ip::address::from_string("192.168.31.102"), 8080 };
-io_context context;
+namespace beast = boost::beast;
+using namespace boost::json;
+namespace http = beast::http;
+namespace net = boost::asio;
+using tcp = boost::asio::ip::tcp;
 
-ip::tcp::acceptor acceptor{ context, endpnt };
+class http_connection : public std::enable_shared_from_this<http_connection> {
+public:
+    http_connection(tcp::socket socket) : socket_(std::move(socket)) {}
 
-std::string getResponce() {
-    std::string content = "\'{\"name\":\"Иван\",\n\"age\":37,\n\"mother\":\n{\"name\":\"Ольга\",\n\"age\":58},\n\"children\":\n[\"Маша\",\n\"Игорь\",\n\"Таня\"],\n\"married\":\ntrue,\n\"dog\":\nnull}\'";
-    std::string responce = "HTTP/1.0 200 OK\r\nContent - length: " + std::to_string(content.size()) + "Connection: close\r\nContent - Type: application/json\r\n\r\n" + content;
-    return responce;
-}
+    void start() {
+        read_request();
+    }
 
-void reader_writer(const std::shared_ptr<ip::tcp::socket> &socket, 
-    std::shared_ptr<std::vector<char>> buffer = std::make_shared<std::vector<char>>(4096)) {
+private:
+    tcp::socket socket_;
+    beast::flat_buffer buffer_{ 4096 };
+    http::request<http::dynamic_body> request_;
+    http::response<http::dynamic_body> response_;
 
-    socket->async_read_some(boost::asio::buffer(*buffer), [socket, buffer](auto error, auto n) {
-        std::cout << "read " << n << ' ' << error << std::endl;
-        if (error) {
-            socket->close();
-            return;
-        }
-        buffer->clear();
-        std::string responce = getResponce();
-        std::shared_ptr<std::vector<char>> res = std::make_shared<std::vector<char>>(responce.begin(), responce.end());
-        socket->async_write_some(boost::asio::buffer(res->data(), res->size()), [socket, buffer](auto error, auto n) {
-            std::cout << "write " << n << ' ' << error << std::endl;
-            socket->close();
-            return;
+    void read_request() {
+        auto self = shared_from_this();
+
+        http::async_read( socket_, buffer_, request_, [self](beast::error_code ec, std::size_t bytes_transferred) {
+                boost::ignore_unused(bytes_transferred);
+                if (!ec)
+                    self->process_request();
         });
+    }
+
+    void process_request() {
+        response_.version(request_.version());
+        response_.keep_alive(false);
+
+        response_.result(http::status::ok);
+        response_.set(http::field::server, "Beast");
+        create_response();
+
+        write_response();
+    }
+
+    void create_response() {
+        object obj;
+        obj["pi"] = 3.141;
+        obj["happy"] = true;
+        obj["name"] = "Boost";
+        response_.set(http::field::content_type, "application/json");
+        beast::ostream(response_.body()) << obj;
+    }
+
+    void write_response() {
+        auto self = shared_from_this();
+
+        response_.content_length(response_.body().size());
+
+        http::async_write(socket_, response_, [self](beast::error_code ec, std::size_t) {
+                self->socket_.close();
+        });
+    }
+};
+
+void http_server(tcp::acceptor& acceptor, tcp::socket& socket) {
+    acceptor.async_accept(socket, [&](beast::error_code ec) {
+        if (!ec)
+            std::make_shared<http_connection>(std::move(socket))->start();
+        http_server(acceptor, socket);
     });
 }
 
-void accept() {
-    std::shared_ptr<ip::tcp::socket> socket = std::make_shared<ip::tcp::socket>(context);
+int main() {
+    try
+    {
+        tcp::endpoint endpnt{ boost::asio::ip::address::from_string("192.168.31.102"), 8080 };
 
-    acceptor.async_accept(*socket, [socket](const auto& error) {
-        std::cout << "accept " << error << std::endl;
-        if (error)
-            return;
-        reader_writer(socket);
-        accept();
-    });
-}
+        net::io_context ioc{ 1 };
+        tcp::acceptor acceptor{ ioc, endpnt };
+        tcp::socket socket{ ioc };
+        http_server(acceptor, socket);
 
-int main()
-{
-    accept();
-    context.run();
-    return 0;
+        ioc.run();
+    }
+    catch (std::exception const& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 }
